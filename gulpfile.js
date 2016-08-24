3b563e5f6897cb1f3735d9efc27b9bd6387cc5aa
sankit acharya <sankit.acharya@gmail.com>
2016-08-24 15:06:17 +0530

    Compress Images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('script',function() {
   gulp.src('js/*.js')
@@ -14,4 +15,10 @@ gulp.task('style',function() {
   .pipe(gulp.dest('css'))
 });
 
-gulp.task('default',['script','style']);
+gulp.task('images',function() {
+  gulp.src('img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('img'))
+});
+
+gulp.task('default',['script','style','images']);
